45062108ed8ceab995da7ad6b09d91333fb4d60a
lcferreira <lucasandradef44@gmail.com>
2025-07-09 16:52:04 -0300

    add ModelViewSet para gerenciar operações CRUD de clientes.

--- clients/views.py
+++ clients/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Client
+from .serializers import ClientSerializer
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+
+class ClientView(viewsets.ModelViewSet):
+    queryset = Client.objects.all()
+    serializer_class = ClientSerializer
+    # permission_classes = [IsAuthenticated]
+
+

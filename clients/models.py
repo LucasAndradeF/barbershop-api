43b6aaf63fc9e560facc25c48469032cde40e964
lcferreira <lucasandradef44@gmail.com>
2025-07-09 16:25:06 -0300

    criado a classe Client

--- clients/models.py
+++ clients/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+import uuid
 
-# Create your models here.
+
+class Client(AbstractUser):
+    id = models.UUIDField(default=uuid.uuid4, primary_key=True, editable=False)
+    phone = models.CharField(max_length=20)
+    email = models.EmailField(max_length=255, unique=True)
+
+    USERNAME_FIELD = email
+    REQUIRED_FIELDS = ['first_name', 'last_name']
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
+
+    class Meta:
+        verbose_name = 'Cliente'
+        verbose_name_plural = 'Clientes'
+ 

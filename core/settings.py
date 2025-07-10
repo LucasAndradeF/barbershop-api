cf215bf4e6ccd472e2e0de98cd11ce4a9ef91a4b
lcferreira <lucasandradef44@gmail.com>
2025-07-10 16:44:08 -0300

    documentação gerada através da lib drf-spectacular contendo redoc, swagger-ui e arquivo .yaml para download

--- core/settings.py
+++ core/settings.py
@@ -134,3 +134,11 @@ REST_FRAMEWORK = {
     ),
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
 }
+
+
+SPECTACULAR_SETTINGS = {
+    'TITLE': 'BARBERSHOP_API',
+    'DESCRIPTION': 'Documentação da API do meu sistema.',
+    'VERSION': '1.0.0',
+    'SERVE_INCLUDE_SCHEMA': False,  
+}
--- core/urls.py
+++ core/urls.py
@@ -1,10 +1,23 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
 
 urlpatterns = [
+    # Rotas da Documentação gerada através da lib "drf-spectacular"
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('api/schema/swagger-ui/',
+         SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('api/schema/redoc/',
+         SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
+
+    # Rota admin para acesso ao painel de administração do DJANGO
     path('admin/', admin.site.urls),
+
+    # Todas as rotas do app clients
     path('api/', include('clients.urls')),
+
+    # Rotas de login e refresh token da aplicação
     path('api/auth/login/', TokenObtainPairView.as_view(), name='login'),
     path('api/auth/refresh/', TokenRefreshView.as_view(), name='refresh_token'),
 ]

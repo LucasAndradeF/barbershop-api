6b0dcf19efddb951f1816fa29875338f2696b891
lcferreira <lucasandradef44@gmail.com>
2025-07-11 13:33:05 -0300

    adicionado ajustes de herança na classe Barber

--- barbers/models.py
+++ barbers/models.py
@@ -1,9 +1,8 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
-import uuid
+from core.models import User
 
 
-class Barber(AbstractUser):
+class Barber(User):
     CHOICES_TYPE_ESPECIALIDADE = (
         ('corte', 'Corte'),
         ('barba', 'Barba'),
@@ -12,15 +11,12 @@ class Barber(AbstractUser):
         ('hidratacao', 'Tratamento Capilar'),
     )
 
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     especialidade = models.CharField(
         choices=CHOICES_TYPE_ESPECIALIDADE, max_length=50, null=False, blank=False)
-    phone = models.CharField(max_length=20)
-    email = models.EmailField(max_length=255, unique=True)
     user_type = models.CharField(max_length=20, default='barber')
 
     def __str__(self):
-        return self.first_name
+        return f'{self.first_name} {self.last_name}'
 
     class Meta:
         verbose_name = 'Barbeiro'

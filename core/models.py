ad3aafe2fabe9af0f2beab2bdca874497faf156d
lcferreira <lucasandradef44@gmail.com>
2025-07-11 13:29:39 -0300

    Refatora modelo User como abstrato para herança de múltiplos perfis

---
+++ core/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+import uuid
+
+
+class User(AbstractUser):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    phone = models.CharField(max_length=20)
+    email = models.EmailField(max_length=255, unique=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username', 'first_name', 'last_name']
+
+    def __str__(self):
+        return f'{self.first_name}'
+    
+    class Meta:
+        abstract = True

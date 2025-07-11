4d32982bb22b68488208740ffeacc0680b072779
lcferreira <lucasandradef44@gmail.com>
2025-07-11 13:30:53 -0300

    refatorado classe Client que agora herda de User

--- clients/models.py
+++ clients/models.py
@@ -1,16 +1,9 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
-import uuid
+from core.models import User
 
 
-class Client(AbstractUser):
-    id = models.UUIDField(default=uuid.uuid4, primary_key=True, editable=False)
-    phone = models.CharField(max_length=20)
-    email = models.EmailField(max_length=255, unique=True)
+class Client(User):
     user_type = models.CharField(max_length=20, default='client')
-
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['username', 'first_name', 'last_name']
 
     def __str__(self):
         return f'{self.first_name} {self.last_name}'
